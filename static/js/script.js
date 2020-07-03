a8d389e94449cdf8fdd408b8cc08589c942ae76d
Toogii <dashdava@uw.edu>
2020-07-03 15:01:58 -0700

    Choosing between Quizes

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,5 @@
 
-var jsQuestions = [
+const jsQuestions = [
   {
     title: 'Who invented JavaScript?',
     choices: ["Douglas Crockford","Sheryl Sandberg","Brendan Eich"],
@@ -28,17 +28,119 @@ var jsQuestions = [
   // etc.
 ];
 
-function jsQuizLandingPage() {
-  
+
+const htmlQuestions = [
+  {
+    title: 'Who invented HTML?',
+    choices: ["Douglas Crockford","Sheryl Sandberg","Brendan Eich"],
+    answer: 'Brendan Eich'
+  },
+  {
+    title: 'Which one of these is a JavaScript package manager?',
+    choices: ["Node.js","TypeScript","npm"],
+    answer: "npm"
+  },
+  {
+    title: 'Which tool can you use to ensure code quality?',
+    choices: ["Angular","jQuery","RequireJS","ESLint"],
+    answer: "ESlint"
+  },
+  {
+    title: 'Commonly used data types DO NOT include:',
+    choices: ['strings', 'booleans', 'alerts', 'numbers'],
+    answer: 'alerts'
+  },
+  {
+    title: 'The condition in an if / else statement is enclosed within ____.',
+    choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
+    answer: 'parentheses'
+  }
+  // etc.
+];
+
+const cssQuestions = [
+  {
+    title: 'Who invented CSS?',
+    choices: ["Douglas Crockford","Sheryl Sandberg","Brendan Eich"],
+    answer: 'Brendan Eich'
+  },
+  {
+    title: 'Which one of these is a JavaScript package manager?',
+    choices: ["Node.js","TypeScript","npm"],
+    answer: "npm"
+  },
+  {
+    title: 'Which tool can you use to ensure code quality?',
+    choices: ["Angular","jQuery","RequireJS","ESLint"],
+    answer: "ESlint"
+  },
+  {
+    title: 'Commonly used data types DO NOT include:',
+    choices: ['strings', 'booleans', 'alerts', 'numbers'],
+    answer: 'alerts'
+  },
+  {
+    title: 'The condition in an if / else statement is enclosed within ____.',
+    choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
+    answer: 'parentheses'
+  }
+  // etc.
+];
+
+function displayAnswers(index) {
+  answerField.innerHTML = "";
+  myQuestions[index].choices.forEach(function(item, i) {
+  var answerEl = document.createElement("p");
+  answerEl.textContent = item;
+  answerEl.setAttribute("class", "answers");
+  answerField.appendChild(answerEl);
+})
+}
+
+function displayQuiz(index) {
+  if (index == myQuestions.length) {
+    return;
+  }
+  else if (index == 0) {
+    startQuizButton.textContent = "Next";
+    var questionEl = document.createElement("h5");
+    questionEl.textContent = myQuestions[index].title;
+    questionEl.setAttribute("class", "questions");
+    questionField.innerHTML = "";
+    questionField.appendChild(questionEl);
+    // Displaying Answers
+    displayAnswers(index);
+  }
+  else {
+    questionField.textContent = myQuestions[index].title;
+    displayAnswers();
+  }
 }
 
 
-var questionField = document.getElementById("question");
-var answerField = document.getElementById("answer");
-var myQuestions = questions;
+function QuizLandingPage() {
+  var subTitle = document.getElementById("quiz");
+  var h3 = document.createElement("h3")
+  h3.innerText = "Javascript Quiz";
+  subTitle.appendChild(h3);
+}
 
-var timeEl = document.querySelector("#time-remaining");
-var mainEl = document.getElementById("main");
+function playQuiz(event) {
+  index = 0;
+  displayQuiz(index);
+}
+
+//Events
+var startQuizButton = document.getElementById("start-next");
+startQuizButton.addEventListener("click", playQuiz);
+// var nextButton = document.getElementById("next");
+// nextButton.addEventListener("click", playQuiz);
+
+
+var myQuestions = jsQuestions;
+var questionField = document.getElementById("quiz");
+var answerField = document.getElementById("answers");
+var timeEl = document.querySelector("#time");
 
 
 
@@ -49,33 +151,54 @@ var mainEl = document.getElementById("main");
 //   questionField.innerText = myQuestions[i].question;
 // }
 
-function setTime() {
-  var timerInterval = setInterval(function() {
-    secondsLeft--;
-    // alert("Hi");
+// function setTime() {
+//   var timerInterval = setInterval(function() {
+//     secondsLeft--;
+//     // alert("Hi");
 
-    timeEl.textContent = secondsLeft;
+//     timeEl.textContent = secondsLeft;
 
-    if(secondsLeft === 0) {
-      clearInterval(timerInterval);
-      // sendMessage();
-    }
+//     if(secondsLeft === 0) {
+//       clearInterval(timerInterval);
+//       // sendMessage();
+//     }
 
-  }, 1000);
-}
-
-
-
-// function sendMessage() {
-//   timeEl.textContent = " ";
-
-//   var imgEl = document.createElement("img");
-
-//   imgEl.setAttribute("src", "images/image_1.jpg");
-//   mainEl.appendChild(imgEl);
-
+//   }, 1000);
 // }
 
 
-setTime();
-i = 0;
+var quizType = document.getElementById("quiz-types");
+quizType.addEventListener("click", function (event) {
+  var element = event.target;
+  
+  console.log(element.textContent);
+  if (element.matches("li")) {
+    switch(element.textContent.split(" ")[0]) {
+      case "HTML":
+        myQuestions = htmlQuestions;
+        alert(myQuestions[0].title)
+
+        break;
+      case "JS":
+        myQuestions = jsQuestions;
+        alert(myQuestions[0].title)
+
+
+        break;
+      case "CSS":
+        myQuestions = cssQuestions;
+        alert(myQuestions[0].title)
+
+        break;
+      default:
+        break;
+    }
+  } 
+})
+
+QuizLandingPage();
+
+
+
+// setTime();
+// i = 0;
