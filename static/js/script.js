34eca56dd44c4c11266d0bc883555b6a62f1a2e0
Toogii <dashdava@uw.edu>
2020-07-03 19:09:46 -0700

    50% Complete

--- static/js/script.js
+++ static/js/script.js
@@ -87,42 +87,107 @@ const cssQuestions = [
   // etc.
 ];
 
+
 function resetPage() {
   answerField.innerHTML = "";
-  startQuizButton.textContent = "Start Quiz";
+  QuizButton.textContent = "Start Quiz";
   var subTitle = document.getElementById("quiz");
   subTitle.innerHTML = "";
 }
 
+function checkAnswer(index) {
+  answer = document.getElementsByClassName("answer");
+  for (i=0;i<answer.length;i++) {
+    
+    if (answer[i].checked) {
+      if (answer[i].parentElement.textContent === myQuestions[index-1].answer) {
+        currentScore += scoreIncrementStep;
+        scoreEl.innerText = currentScore;
+        
+      }
+    }
+  }
+  
+}
+
+function endQuiz() {
+  resetPage();
+  var iniTials = document.createElement("input");
+  iniTials.id = "initials-input";
+  iniTials.name = "initials-input";
+  iniTials.type = "text";
+  iniTials.placeholder = "Your Initials";
+  iniTials.setAttribute("class", "center");
+  var scoreContainer = document.createElement("div");
+  scoreContainer.type = "number";
+  scoreContainer.textContent = `Your Score: ${currentScore}`;
+  questionField.appendChild(iniTials);
+  answerField.appendChild(scoreContainer);
+  QuizButton.textContent = "Submit";
+  QuizButton.type = "submit";
+  QuizButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    event.stopPropagation();
+    var playerNameInput = document.querySelector("#initials-input");
+    console.log(playerNameInput.value.trim());
+    if (playerNameInput) {
+      console.log(playerNameInput.value);
+    }
+    else {
+      alert("Enter Your Initials and click Submit");
+    }
+  })
+  return;
+
+}
+
 function displayAnswers(index) {
   answerField.innerHTML = "";
+  var quizForm = document.createElement("form");
+  quizForm.role = "form";
+  quizForm.id="quizfrom";
+
   myQuestions[index].choices.forEach(function(item, i) {
-  var answerEl = document.createElement("p");
-  answerEl.textContent = item;
-  answerEl.setAttribute("class", "answers");
-  answerField.appendChild(answerEl);
-})
+    var answerLabel = document.createElement("label");
+    answerLabel.setAttribute("class", "radiocontainer");
+    answerLabel.name = "quiz";
+    var answerInput = document.createElement("input");
+    answerInput.type = "radio";
+    answerInput.name = "answer" + i;
+    answerInput.setAttribute("class", "answer");
+    answerInput.id = "answer" + i;
+    answerLabel.textContent = item;
+    answerLabel.appendChild(answerInput);
+    quizForm.appendChild(answerLabel);
+  }
+  )
+  answerField.appendChild(quizForm);
+  return;
 }
 
 function displayQuiz(index) {
-  
-  if (index == myQuestions.length) {
+  if (index > (myQuestions.length-1)) {
+    endQuiz();
     return;
   }
   else if (index == 0) {
-    startQuizButton.textContent = "Next";
+    QuizButton.textContent = "Next";
   }
-  var questionEl = document.createElement("h5");
+  var questionNumEl = document.createElement("h3");
+  questionNumEl.textContent = `Question ${index+1} out of ${myQuestions.length}`
+  
+  var questionEl = document.createElement("p");
   questionEl.textContent = myQuestions[index].title;
-  questionEl.setAttribute("class", "questions");
   questionField.innerHTML = "";
+  questionField.appendChild(questionNumEl);
   questionField.appendChild(questionEl);
+  questionField.setAttribute("class", "left");
   // Displaying Answers
   displayAnswers(index);
 }
 
 
-function quizLandingPage() {
+function quizLandingPage(index) {
   resetPage();
   var subTitle = document.getElementById("quiz");
   var h3 = document.createElement("h3")
@@ -138,18 +203,13 @@ function quizLandingPage() {
 }
 
 function playQuiz(event) {
+  if (index > 0) {
+    checkAnswer(index);
+  }
   displayQuiz(index);
   index++;
 }
 
-index = 0;
-var startQuizButton = document.getElementById("start-next");
-startQuizButton.addEventListener("click", playQuiz);
-
-var myQuestions = jsQuestions;
-var questionField = document.getElementById("quiz");
-var answerField = document.getElementById("answers");
-var timeEl = document.querySelector("#time");
 
 
 
@@ -180,24 +240,26 @@ var quizTypeEl = document.getElementById("quiz-types");
 quizTypeEl.addEventListener("click", function (event) {
   var element = event.target;
   
-  console.log(element.textContent);
   if (element.matches("li")) {
     switch(element.textContent.split(" ")[0]) {
       case "HTML":
         myQuestions = htmlQuestions;
         alert(myQuestions[0].title);
         quizTypeVar = "HTML";
+        index = 0;
         quizLandingPage();
         break;
       case "JS":
         myQuestions = jsQuestions;
         alert(myQuestions[0].title)
         quizTypeVar = "Javascript";
+        index = 0;
         quizLandingPage();
         break;
       case "CSS":
         myQuestions = cssQuestions;
         alert(myQuestions[0].title)
+        index = 0;
         quizTypeVar = "CSS";
         quizLandingPage();
 
@@ -208,9 +270,24 @@ quizTypeEl.addEventListener("click", function (event) {
   } 
 })
 
-quizLandingPage();
 
 
+
+index = 0;
+var QuizButton = document.getElementById("start-next-finish");
+QuizButton.addEventListener("click", playQuiz);
+
+var myQuestions = jsQuestions;
+var questionField = document.getElementById("quiz");
+var answerField = document.getElementById("answers");
+var timeEl = document.querySelector("#time");
+var scoreEl = document.querySelector("#score");
+
+var currentScore = 0;
+var highestScore = 100;
+var scoreIncrementStep = Math.floor(highestScore/myQuestions.length);
+var answer;
+quizLandingPage(0);
 
 // setTime();
 // i = 0;
